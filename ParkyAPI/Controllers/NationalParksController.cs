8690ab04604fed6639bab56a4904dc2dca376d4b
Anindya Sarkar <anindya.stcet@gmail.com>
2021-07-05 14:51:23 +0530

    GetNationalPark returning data , except Dto linked

---
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParksController : ControllerBase
+    {
+        private readonly INationalParkRepository _npRepo;
+        private readonly IMapper _mapper;
+
+        public NationalParksController(INationalParkRepository npRepo,IMapper mapper)
+        {
+            _npRepo = npRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetNationalParks()
+        {
+            var obj = _npRepo.GetNationalParks();
+            return Ok(obj);
+
+            // inspite of exposing Model returing Dto 
+        }
+    }
+}
